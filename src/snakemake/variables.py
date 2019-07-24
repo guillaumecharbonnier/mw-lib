38c3286465f61f4ef231622c90ef298ed956a427
Guillaume Charbonnier <guillaume.charbonnier@outlook.com>
2019-07-24 17:31:12 +0200

    mv CHROMDET_OUTPUT_FILES from mw-thymus to mw-lib

--- src/snakemake/variables.py
+++ src/snakemake/variables.py
@@ -49,3 +49,11 @@ TAR_CONTENT_HMCAN_DIFF_TEST_EXAMPLE=[
     "data/GC_profile_100KbWindow_Mapp76_hg19.cnp",
     "control_C2.sam"]
 
+#samples_collapsed_{file},file=CHROMDET_OUTPUT_FILES
+CHROMDET_OUTPUT_FILES=[
+    "_filtered_CDRs.tsv",
+    "_filtered_chromatin_space.tsv",
+    "_filtered.fa",
+    "_filtered.s3",
+    "_filtered.tab",
+    ".tab"]
