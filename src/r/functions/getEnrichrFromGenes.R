549575d90084e4591209f77f4219dfe79fdfa09d
Guillaume Charbonnier <guillaume.charbonnier@outlook.com>
2021-01-07 23:19:29 +0100

    improve deseq2 template and functions

---
+++ src/r/functions/getEnrichrFromGenes.R
@@ -0,0 +1,17 @@
+getEnrichrUrlFromGenes <- function(genes) {
+  req.body <- list(
+    list=paste(
+      genes,
+      collapse="\n"
+    )
+  )
+  post.req <- httr::POST(
+    "http://maayanlab.cloud/Enrichr/addList", 
+    encode="multipart", 
+    body=I(req.body)
+  )
+  url <- paste0(
+    "https://amp.pharm.mssm.edu/Enrichr/enrich?dataset=",
+    jsonlite::fromJSON(content(post.req, "text"))$shortId
+  )
+}
--- src/rmd/templates/knitTemplates.R
+++ src/rmd/templates/knitTemplates.R
@@ -1,12 +1,41 @@
 # These functions define default arguments for Rmd templates defined in this folder.
 
+
+#' @params cols is a vector of column names to keep in the produced DataTable. 
 knitPostDeseq2Template <- function(
-                               d = vsd,
-                               r = res,
-                               p_cutoff = 0.05,
-                               FC_cutoff = 2,
-                               output_dir = book_from_rmd,
-                               chunk_label_prefix = opts_current$get("label")) {
+  d = vsd,
+  r = res,
+  p_cutoff = 0.05,
+  FC_cutoff = 2,
+  cols = c(
+    "ensembl_gene_id",
+    "hugo_symbol",
+    "baseMean",
+    "log2FoldChange",
+    "pvalue",
+    "padj"
+  ),
+  output_dir = book_from_rmd,
+  chunk_label_prefix = opts_current$get("label")) {
+  
+  required_r_cols <- c(
+    "baseMean",
+    "log2FoldChange",
+    "pvalue",
+    "padj"
+  )
+
+  if (class(d) != "DESeqDataSet") {
+    error("d should be a 'DESeqDataSet' object")
+  }
+  if (class(r) != "DESeqResults") {
+    error("r should be a 'DESeqResults' object produced for example by lfcShrink()")
+  }
+  if (! "hugo_symbol" %in% names(rowData(d))) {
+    warning("hugo_symbol column is missing. It can be useful to produce enrichment analyses.")
+  }
+  
+
   src <- knitr::knit_expand("templates/postdeseq2.Rmd")
   res <- knitr::knit_child(
     text = unlist(src),
