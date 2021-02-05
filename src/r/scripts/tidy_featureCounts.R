62eaa461e3076f2b8d7036b255b9102c58b19fbc
Guillaume Charbonnier <guillaume.charbonnier@outlook.com>
2021-02-06 00:47:19 +0100

    add r tidy featureCounts

---
+++ src/r/scripts/tidy_featureCounts.R
@@ -0,0 +1,32 @@
+library(data.table)
+library(edgeR)
+
+d <- fread(snakemake@input[["tsv"]])
+
+d[, c("Chr", "Start", "End", "Strand", "Length") := NULL]
+
+setnames(
+  d,
+  old = names(d[,-1]),
+  new = gsub(
+    pattern = "^.*/|.bam$",
+    replacement = "",
+    x = names(d[,-1])
+  )
+)
+
+fwrite(
+  x = d,
+  file = snakemake@output[["raw"]]
+)
+
+m <- rpkm(d[,-1])
+d <- data.table(
+  d[,1],
+  m
+)
+
+fwrite(
+  x = d,
+  file = snakemake@output[["rpkm"]]
+)
