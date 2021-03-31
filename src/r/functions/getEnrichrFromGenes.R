19b1fec8fb5809c7c1b584b440b0d1e72e6b34e9
Guillaume Charbonnier <guillaume.charbonnier@outlook.com>
2021-03-31 10:40:11 +0200

    fix edgecase for query to Enrichr

--- src/r/functions/getEnrichrFromGenes.R
+++ src/r/functions/getEnrichrFromGenes.R
@@ -1,20 +1,24 @@
 getEnrichrUrlFromGenes <- function(genes) {
-  req.body <- list(
-    list = paste(
-      genes,
-      collapse = "\n"
+  if (length(genes) == 0) {
+    return("No gene in this query to Enrichr")
+  } else {
+    req.body <- list(
+      list = paste(
+        genes,
+        collapse = "\n"
+      )
     )
-  )
-  post.req <- httr::POST(
-    "http://maayanlab.cloud/Enrichr/addList",
-    encode = "multipart",
-    body = I(req.body)
-  )
-  url <- paste0(
-    "https://maayanlab.cloud/Enrichr/enrich?dataset=",
-    jsonlite::fromJSON(httr::content(
-      post.req,
-      "text"
-    ))$shortId
-  )
+    post.req <- httr::POST(
+      "http://maayanlab.cloud/Enrichr/addList",
+      encode = "multipart",
+      body = I(req.body)
+    )
+    url <- paste0(
+      "https://maayanlab.cloud/Enrichr/enrich?dataset=",
+      jsonlite::fromJSON(httr::content(
+        post.req,
+        "text"
+      ))$shortId
+    )
+  }
 }
