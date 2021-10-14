c276691b208dec92956d2b12b96bb2557e70385e
Guillaume Charbonnier <guillaume.charbonnier@outlook.com>
2021-10-14 18:02:14 +0200

    Add R function for ComplexHeatmap palette

--- src/r/functions/plotPcaIndFacetsForEachCov.R
+++ src/r/functions/plotPcaIndFacetsForEachCov.R
@@ -26,3 +26,41 @@ plotPcaIndFacetsForEachCov <- function(
     common.legend = FALSE
   )
 }
+
+plotUmapIndFacetsForEachCov <- function(
+  d = d_umap,
+  d_cov = d_covariates,
+  n_row = 1,
+  n_col = ncol(d_covariates)
+) {
+  d_umap <- data.table(
+    data.table(d$layout),
+    as.data.table(d_cov)
+  )
+
+  pl <- list()
+  plots <- list()
+  for (covariate_name in names(d_cov)) {
+    plot <- ggplot(
+      d_umap,
+      aes_string(
+        x = "V1",
+        y = "V2",
+        color = covariate_name,
+        shape = covariate_name
+      )
+    )
+    plot <- plot + scale_shape_manual(values=1:nlevels(factor(d_cov[[covariate_name]])))
+    plot <- plot + geom_point()
+    #plot <- plot + ggtitle(NULL)
+    plots[[covariate_name]] <- plot
+  }
+
+  ggarrange(
+    plotlist = plots,
+    ncol = n_col,
+    nrow = n_row,
+    align = "hv",
+    common.legend = FALSE
+  )
+}
